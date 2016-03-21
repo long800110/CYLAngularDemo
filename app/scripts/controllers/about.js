97e18899c34e1a1366ed70b0aa200c921c8a5bd3
CuiYuanlong <long_800110@163.com>
2016-03-21 20:55:54 +0800

    project init for CYLAngularDemo

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name cylangularDemoApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the cylangularDemoApp
+ */
+angular.module('cylangularDemoApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name cylangularDemoApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the cylangularDemoApp
+ */
+angular.module('cylangularDemoApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
